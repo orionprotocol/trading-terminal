9a3ab0c796963b589b5ec60fc0a961f6f5ced878
Fahrutdinov Nail <nail94.itis@gmail.com>
2019-01-26 16:22:45 +0300

    change java opts

--- operations/docker/production/production/setenv.sh
+++ operations/docker/production/production/setenv.sh
@@ -1,7 +1,8 @@
+export JAVA_OPTS="
 -Dcom.sun.management.jmxremote
 -Dcom.sun.management.jmxremote.local.only=false
 -Dcom.sun.management.jmxremote.authenticate=false
 -Dcom.sun.management.jmxremote.port=9010
 -Dcom.sun.management.jmxremote.rmi.port=9010
 -Djava.rmi.server.hostname=0.0.0.0
--Dcom.sun.management.jmxremote.ssl=false
+-Dcom.sun.management.jmxremote.ssl=false"
