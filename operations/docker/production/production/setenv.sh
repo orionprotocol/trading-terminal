514f03d3700b34835be551554754b839c7fa17e7
Fahrutdinov Nail <nail94.itis@gmail.com>
2019-01-26 14:45:57 +0300

    change jmx settings

--- operations/docker/production/production/setenv.sh
+++ operations/docker/production/production/setenv.sh
@@ -1,4 +1,5 @@
 -Dcom.sun.management.jmxremote
--Dcom.sun.management.jmxremote.port=9999
 -Dcom.sun.management.jmxremote.authenticate=false
 -Dcom.sun.management.jmxremote.ssl=false
+-Dcom.sun.management.jmxremote.port=9090
+-Dcom.sun.management.jmxremote.rmi.port=9090
