feb699d55c6c960e8f9367db5da397b8eaabbdab
Fahrutdinov Nail <nail94.itis@gmail.com>
2019-01-26 15:48:18 +0300

    change hostname

--- operations/docker/production/production/setenv.sh
+++ operations/docker/production/production/setenv.sh
@@ -3,5 +3,5 @@
 -Dcom.sun.management.jmxremote.authenticate=false
 -Dcom.sun.management.jmxremote.port=9010
 -Dcom.sun.management.jmxremote.rmi.port=9010
--Djava.rmi.server.hostname=***REMOVED***
+-Djava.rmi.server.hostname=172.17.0.2
 -Dcom.sun.management.jmxremote.ssl=false
