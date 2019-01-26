423f54d6904136bcee01a11901ce4930cc969e52
Fahrutdinov Nail <nail94.itis@gmail.com>
2019-01-26 14:34:58 +0300

    add port for jmx

--- operations/docker/production/production/build-run.sh
+++ operations/docker/production/production/build-run.sh
@@ -4,6 +4,7 @@ IMAGE_NAME=exchange_notifier
 IMAGE_PATH=.
 PORT=80
 SSH_PORT=443
+JMX_PORT=9999
 
 sudo docker build -t $IMAGE_NAME $IMAGE_PATH
-sudo docker run -d -p $PORT:$PORT -p $SSH_PORT:$SSH_PORT $IMAGE_NAME
+sudo docker run -d -p $PORT:$PORT -p $SSH_PORT:$SSH_PORT -p $JMX_PORT:$JMX_PORT $IMAGE_NAME
