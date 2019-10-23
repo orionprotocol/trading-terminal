9dc5f0f9e03e4a5729b2d1fafff772950b041fa6
Juan <navascruzf@gmail.com>
2019-10-23 11:50:34 -0400

    update charting_library, add order submit to 'api/order'

--- app/service/WanchainOrder.js
+++ app/service/WanchainOrder.js
@@ -6,7 +6,7 @@ class WanchainOrder {
 	static defaultMatcherFee = 300000;
 	static defaultExpiration = 29 * 24 * 60 * 60 * 1000;
 	static matcherPublicKey = '0x1e7b4238bab7d3f5e8d09a18b44c295228b80643';
-	//static orionUrl = `http://${window.location.hostname}:3001`;
+	static orionUrl = `http://${window.location.hostname}:3001`;
 	// static orionUrl = "https://demo.orionprotocol.io:2083";
 	// baseAsset, es el que el cliente tiene; quoteAsset, es el activo que quiere
 	// en compra tiene c2 y quiere c1
@@ -30,7 +30,7 @@ class WanchainOrder {
 			quotetAsset = tokensAddress[symbols[0].toUpperCase()];
 		}
 
-		const params = {
+		const order = {
 			senderAddress: senderAddress,
 			matcherAddress: WanchainOrder.matcherPublicKey,
 			baseAsset: baseAsset,
@@ -38,16 +38,48 @@ class WanchainOrder {
 			matcherFeeAsset: baseAsset,
 			amount: amount,
 			price: price,
-			matcherFee: 300000,
+			matcherFee: 350000000,
 			nonce: nowTimestamp,
-			expiration: nowTimestamp + 29 * 24 * 60 * 60 * 1000,
+			expiration: nowTimestamp + 29 * 24 * 60 * 60,
 			side: sideBoolean //true = buy, false = sell
 		};
 
-		let signedOrder = await signOrder(params);
-		console.log('----- Message: ', hashOrder(params));
+		let signedOrder = await signOrder(order);
+
+		order.signature = signedOrder;
+
+		console.log(order);
+		console.log('----- Message: ', hashOrder(order));
 		console.log('----- Signature: ', signedOrder);
 		console.log('----- Signed By: ', senderAddress);
+
+		fetch(`${WanchainOrder.orionUrl}/api/order`, {
+			credentials: 'same-origin',
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ order })
+		})
+			.then(results => {
+				return results.json();
+			})
+			.then(data => {
+				console.log(data);
+				// if (data.code && data.code == "1010") {
+				// 	Toastr.showError(
+				// 		"No brokers were found to execute your order"
+				// 	);
+				// 	return;
+				// } else if (data.code && data.code == "1000") {
+				// 	Toastr.showError("Not enough tradable balance.");
+				// 	return;
+				// } else {
+				// 	Toastr.showSuccess("Order has been created");
+				// 	this.props.loadOrderHistory(this.props.pair);
+				// }
+			});
 
 		return signedOrder;
 	}
