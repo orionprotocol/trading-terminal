5a5e4a8d4d1b0ad23fcc6db0d893bb6d1f5c54f7
Juan Navas <navascruzf@gmail.com>
2020-02-20 10:17:41 -0400

    fixed bug on history page

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,58 +1,74 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import TopMenu from '../components/TopMenu';
 import Sidebar from '../components/Sidebar';
 import axios from 'axios';
 import Deposits from '../components/History/Deposits';
 import Withdraws from '../components/History/Withdraws';
-import { useDispatch } from 'react-redux';
-
-let address = '',
-	url = '';
-
-if (window.wan3) {
-	let account = localStorage.getItem('currentAccount');
-
-	if (account && account !== '') {
-		address = window.wan3.toChecksumAddress();
-		url = process.env.REACT_APP_ORION_WAN + '/api/history/' + address;
-	}
-}
+import { useDispatch, useSelector } from 'react-redux';
 
 function History() {
 	const dispatch = useDispatch();
 
+	const [ url, setUrl ] = useState('');
+	const { wanmaskConnected, metamaskConnected } = useSelector((state) => state.wallet);
+
 	const setDeposits = useCallback((data) => dispatch({ type: 'SetDeposits', payload: data }), [ dispatch ]);
 	const setWithdrawls = useCallback((data) => dispatch({ type: 'SetWithdrawls', payload: data }), [ dispatch ]);
 
-	useEffect((_) => {
-		if (url !== '') {
-			axios
-				.get(url)
-				.then((res) => {
-					if (res.data.length > 0) {
-						let w = [],
-							d = [];
+	useEffect(
+		(_) => {
+			if (wanmaskConnected) {
+				let account = localStorage.getItem('currentAccount');
 
-						res.data.map((e) => {
-							if (e.type === 'deposit') {
-								return d.push(e);
-							} else if (e.type === 'withdrawl') {
-								return w.push(e);
-							}
-							return e;
-						});
+				if (account && account !== '') {
+					const address = window.wan3.toChecksumAddress();
+					setUrl(process.env.REACT_APP_ORION_WAN + '/api/history/' + address);
+				}
+			}
+			if (metamaskConnected) {
+				const { ethereum, web3 } = window;
 
-						setDeposits(d);
-						setWithdrawls(w);
-					}
-				})
-				.catch((err) => {
-					if (err.response) console.log(err.response.data);
-					else console.log(err);
-				});
-		}
+				if (!ethereum) ethereum.enable();
+				const address = web3.toChecksumAddress(ethereum.selectedAddress);
+				setUrl(process.env.REACT_APP_ORION_WAN + '/api/history/' + address);
+			}
+		},
+		[ wanmaskConnected, metamaskConnected ]
+	);
+
+	useEffect(
+		(_) => {
+			if (url !== '') {
+				console.log(url);
+				axios
+					.get(url)
+					.then((res) => {
+						if (res.data.length > 0) {
+							let w = [],
+								d = [];
+
+							res.data.map((e) => {
+								if (e.type === 'deposit') {
+									return d.push(e);
+								} else if (e.type === 'withdrawl') {
+									return w.push(e);
+								}
+								return e;
+							});
+
+							setDeposits(d);
+							setWithdrawls(w);
+						}
+					})
+					.catch((err) => {
+						if (err.response) console.log(err.response.data);
+						else console.log(err);
+					});
+			}
+		},
 		//eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+		[ url ]
+	);
 
 	return (
 		<div>
