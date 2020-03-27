afad41b7a7cc310acc4a6ec50e96374f2473de0a
Juan Navas <navascruzf@gmail.com>
2020-03-27 10:59:51 -0400

    fixed bug on history page withdrawals

--- src/pages/History.js
+++ src/pages/History.js
@@ -7,85 +7,107 @@ import Withdraws from '../components/History/Withdraws';
 import { useDispatch, useSelector } from 'react-redux';
 
 function History() {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const [ url, setUrl ] = useState('');
-	const { wanmaskConnected, metamaskConnected } = useSelector((state) => state.wallet);
+    const [url, setUrl] = useState('');
+    const { wanmaskConnected, metamaskConnected } = useSelector(
+        state => state.wallet
+    );
 
-	const setDeposits = useCallback((data) => dispatch({ type: 'SetDeposits', payload: data }), [ dispatch ]);
-	const setWithdrawls = useCallback((data) => dispatch({ type: 'SetWithdrawls', payload: data }), [ dispatch ]);
+    const setDeposits = useCallback(
+        data => dispatch({ type: 'SetDeposits', payload: data }),
+        [dispatch]
+    );
+    const setWithdrawls = useCallback(
+        data => dispatch({ type: 'SetWithdrawls', payload: data }),
+        [dispatch]
+    );
 
-	useEffect(
-		(_) => {
-			if (wanmaskConnected) {
-				let account = localStorage.getItem('currentAccount');
+    useEffect(
+        _ => {
+            if (wanmaskConnected) {
+                let account = localStorage.getItem('currentAccount');
 
-				if (account && account !== '') {
-					const address = window.wan3.toChecksumAddress();
-					setUrl(process.env.REACT_APP_ORION_WAN + '/api/history/' + address);
-				}
-			}
-			if (metamaskConnected) {
-				const { ethereum, web3 } = window;
+                if (account && account !== '') {
+                    const address = window.wan3.toChecksumAddress();
+                    setUrl(
+                        process.env.REACT_APP_ORION_WAN +
+                            '/api/history/' +
+                            address
+                    );
+                }
+            }
+            if (metamaskConnected) {
+                const { ethereum, web3 } = window;
 
-				if (!ethereum) ethereum.enable();
-				const address = web3.toChecksumAddress(ethereum.selectedAddress);
-				setUrl(process.env.REACT_APP_ORION_WAN + '/api/history/' + address);
-			}
-		},
-		[ wanmaskConnected, metamaskConnected ]
-	);
+                if (!ethereum) ethereum.enable();
+                const address = web3.toChecksumAddress(
+                    ethereum.selectedAddress
+                );
+                setUrl(
+                    process.env.REACT_APP_ORION_WAN + '/api/history/' + address
+                );
+            }
+        },
+        [wanmaskConnected, metamaskConnected]
+    );
 
-	useEffect(
-		(_) => {
-			if (url !== '') {
-				console.log(url);
-				axios
-					.get(url)
-					.then((res) => {
-						if (res.data.length > 0) {
-							let w = [],
-								d = [];
+    useEffect(
+        _ => {
+            if (url !== '') {
+                console.log(url);
+                axios
+                    .get(url)
+                    .then(res => {
+                        if (res.data.length > 0) {
+                            let w = [],
+                                d = [];
 
-							res.data.map((e) => {
-								if (e.type === 'deposit') {
-									return d.push(e);
-								} else if (e.type === 'withdrawl') {
-									return w.push(e);
-								}
-								return e;
-							});
+                            res.data.map(e => {
+                                if (e.type === 'deposit') {
+                                    return d.push(e);
+                                } else if (
+                                    e.type === 'withdrawl' ||
+                                    e.type === 'withdrawal'
+                                ) {
+                                    return w.push(e);
+                                }
+                                return e;
+                            });
 
-							setDeposits(d);
-							setWithdrawls(w);
-						}
-					})
-					.catch((err) => {
-						if (err.response) console.log(err.response.data);
-						else console.log(err);
-					});
-			}
-		},
-		//eslint-disable-next-line react-hooks/exhaustive-deps
-		[ url ]
-	);
+                            console.log('deposits ', d);
+                            console.log('withdrawls ', w);
 
-	return (
-		<div>
-			<TopMenu />
-			<div className="history">
-				<Sidebar />
+                            setDeposits(d);
+                            setWithdrawls(w);
+                        }
+                    })
+                    .catch(err => {
+                        if (err.response) console.log(err.response.data);
+                        else console.log(err);
+                    });
+            }
+        },
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+        [url]
+    );
 
-				<div className="my-container">
-					<h1>Transaction History</h1>
-					<div className="my-row">
-						<Deposits />
-						<Withdraws />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <TopMenu />
+            <div className="history">
+                <Sidebar />
+
+                <div className="my-container">
+                    <h1>Transaction History</h1>
+                    <div className="my-row">
+                        <Deposits />
+                        <Withdraws />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default History;
