40f67c0ab71e914e6f73e834416fb1f951a38c32
Juan Navas <navascruzf@gmail.com>
2020-04-20 13:56:46 -0400

    updated Swap

--- src/components/Swap/index.js
+++ src/components/Swap/index.js
@@ -1,61 +1,83 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import './swapSelector.scss'
-import CoinPrice from './priceOfCoins'
-import SwapSelectors from './swapSelector'
+import './swapSelector.scss';
+import CoinPrice from './priceOfCoins';
+import SwapSelectors from './swapSelector';
 import { EthereumOrder } from '../../services/EthereumOrder';
 import openNotification from '../../components/Notification';
 const index = memo(() => {
-    const { mode } = useSelector(state => state.general);
+    const { mode, assets: allAssets } = useSelector(state => state.general);
     const balances = useSelector(state => state.balances);
     const { assets } = useSelector(state => state.wallet);
     const { metamaskConnected, fortmaticConnected } = useSelector(
         state => state.wallet
     );
-    const { tickers } = useSelector(
-        state => state.general
-    );
+    const { tickers } = useSelector(state => state.general);
     /* console.log(assets,balances.contractBalances,metamaskConnected,fortmaticConnected,tickers) */
     const [swapCoins, setswapCoins] = useState({
         from: 'ETH',
-        to: 'WBTC'
-    })
+        to: 'WBTC',
+    });
     const [swapValue, setswapValue] = useState({
         from: 0,
-        to: 0
-    })
-      console.log(assets,tickers, "tickers") 
-    const [errorMessage, seterrorMessage] = useState({ error: false, msg: '' })
+        to: 0,
+    });
+    // console.log(assets, tickers, 'tickers');
+    const [errorMessage, seterrorMessage] = useState({ error: false, msg: '' });
+    const [symbolsB, setSymbolsB] = useState([]);
 
     const setTypeOfTrade = () => {
         switch (swapCoins.from) {
-            case "WBTC":
-                return 'buy'
-            case "USDT":
-                return 'buy'
+            case 'WBTC':
+                return 'buy';
+            case 'USDT':
+                return 'buy';
             default:
                 return 'sell';
         }
-    }
+    };
+
+    useEffect(
+        _ => {
+            if (allAssets.assets1 && allAssets.assets1.length > 0) {
+                const { assets1 } = allAssets;
+
+                const symbsb = assets1.map(e => {
+                    if (assets[e.toUpperCase()]) {
+                        return assets[e.toUpperCase()];
+                    } else {
+                        return e;
+                    }
+                });
+
+                setSymbolsB(symbsb);
+                console.log('symbsb', symbsb);
+
+                const regex = symbsb.join('|');
+                console.log(regex);
+            }
+        },
+        [allAssets, assets]
+    );
 
     const setSymbols = () => {
         let symbolA, symbolB;
+
+        let regex = symbolsB.join('|');
+
         if (/USDT|WBTC/i.test(swapCoins.from)) {
-            symbolA = swapCoins.to
-            symbolB = swapCoins.from
+            symbolA = swapCoins.to;
+            symbolB = swapCoins.from;
         } else if (/USDT|WBTC/i.test(swapCoins.to)) {
-            symbolA = swapCoins.from
-            symbolB = swapCoins.to
+            symbolA = swapCoins.from;
+            symbolB = swapCoins.to;
         }
-        return [symbolA, symbolB]
-    }
+        return [symbolA, symbolB];
+    };
 
-    const setPrice = () => {
-
-    }
+    const setPrice = () => {};
 
     const swap = async () => {
-
         try {
             /*    console.log(orderSymbols, type.trade, price, values.amount); */
 
@@ -63,20 +85,22 @@ const index = memo(() => {
 
             if (fortmaticConnected) {
                 let ethereumOrder = new EthereumOrder('fortmatic');
-                ethereumOrderMessage = await ethereumOrder.toEthereumOrder(
+                ethereumOrderMessage = await ethereumOrder
+                    .toEthereumOrder
                     /*  setSymbols(),
                      setTypeOfTrade(),
                      price,
                      values.amount */
-                );
+                    ();
             } else if (metamaskConnected) {
                 let ethereumOrder = new EthereumOrder('metamask');
-                ethereumOrderMessage = await ethereumOrder.toEthereumOrder(
+                ethereumOrderMessage = await ethereumOrder
+                    .toEthereumOrder
                     /*  setSymbols(),
                     setTypeOfTrade(),
                     price,
                     values.amount */
-                );
+                    ();
             }
 
             /*       loadOrderHistory(); */
@@ -89,27 +113,33 @@ const index = memo(() => {
                         setTotal(0); */
 
             openNotification({
-                message: ethereumOrderMessage
+                message: ethereumOrderMessage,
             });
         } catch (e) {
             console.log('error', e);
 
             if (e.msg) {
                 openNotification({
-                    message: e.msg
+                    message: e.msg,
                 });
             }
         }
-
-    }
+    };
 
     return (
-        <div className={`swap-container ${mode === 'Light' ? "" : "dark-mode"}`}>
+        <div
+            className={`swap-container ${mode === 'Light' ? '' : 'dark-mode'}`}
+        >
             <div className="swap-card">
-                <SwapSelectors swapCoins={swapCoins} setswapCoins={setswapCoins} swapValue={swapValue} setswapValue={setswapValue} />
+                <SwapSelectors
+                    swapCoins={swapCoins}
+                    setswapCoins={setswapCoins}
+                    swapValue={swapValue}
+                    setswapValue={setswapValue}
+                />
                 <CoinPrice swapCoins={swapCoins} />
 
-                <button className='swap-button' onClick={swap}>
+                <button className="swap-button" onClick={swap}>
                     <h6>SWAP</h6>
                 </button>
             </div>
