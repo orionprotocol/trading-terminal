2d48a524158021b1acfbe0d49bffcf5706b33392
raycoga <capram94@gmail.com>
2020-05-06 14:19:00 -0400

    finish the new profit section *its necessary to make yarn to install the dependencies *only left change the style, in resposive and the light mode

--- src/components/Home/Exchange/YourProfit/index.js
+++ src/components/Home/Exchange/YourProfit/index.js
@@ -2,39 +2,74 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import './index.css';
+import Carousel,{ consts } from 'react-elastic-carousel'
 
 const urlBase = process.env.REACT_APP_BACKEND;
 
-const YourProfit = (pros) => {
+const YourProfit = () => {
 	const { symbol, qtyForm, sideForm } = useSelector((state) => state.general);
-	const [ binance, setBinance ] = useState({ pct: 0, btc: 0 });
-	const [ poloniex, setPoloniex ] = useState({ pct: 0, btc: 0 });
-	const [ bittrex, setBittrex ] = useState({ pct: 0, btc: 0 });
+	const [profits, setProfits] = useState('');
+
+	const createProfits = profits => {
+		return profits.map((res, key) => {
+			return (
+
+				<div key={key} className="exchange">
+					<div className="name">
+						<span
+							style={{
+								color: `rgb(240, ${Math.random()*(240-100)+100}, ${Math.random()*(240-100)+100})`,
+								fontWeight: 'bold'
+							}}
+						>
+							{res.name.toUpperCase()}
+						</span>
+					</div>
+					<div className="numbers" style={{
+								color: 'white',
+								fontWeight: 'bold'
+							}}>
+						<span>+ {res.benefitPct} % </span>
+						<span>+ {res.benefitBtc} BTC</span>
+					</div>
+				</div>
+/* 
+				<div key={key} style={{ color: 'white' }}></div>
+
+ */
+				
+			)
+		})
+	}
 
 	const loadBenefits = () => {
-		let url =
-			urlBase +
-			'/api/v1/order-benefits?symbol={symbol}&ordQty={ordQty}&side={side}'
-				.replace('{symbol}', symbol)
-				.replace('{ordQty}', qtyForm)
-				.replace('{side}', sideForm);
-
+		let url = `${urlBase}/api/v1/order-benefits?symbol=${symbol}&ordQty=${qtyForm}&side=${sideForm}`
+		let aux = [], result;
 		axios
 			.get(url)
 			.then((res) => {
-				 console.log(res.data);
-				setBinance({
-					pct: res.data.binance.benefitPct,
-					btc: res.data.binance.benefitBtc
+				result = res.data
+				for (let key in result) {
+					aux.push({
+						name: key,
+						benefitBtc: parseFloat(result[key].benefitBtc),
+						benefitPct: parseFloat(result[key].benefitPct)
+					})
+				}
+				console.log(aux)
+				aux = aux.sort(function (a, b) {
+					if (b.benefitBtc > a.benefitBtc) {
+						return 1;
+					}
+					if (b.benefitBtc < a.benefitBtc) {
+						return -1;
+					}
+					// a must be equal to b
+					return 0;
 				});
-				setPoloniex({
-					pct: res.data.poloniex.benefitPct,
-					btc: res.data.poloniex.benefitBtc
-				});
-				setBittrex({
-					pct: res.data.bittrex.benefitPct,
-					btc: res.data.bittrex.benefitBtc
-				});
+				console.log(aux)
+		
+				setProfits(createProfits(aux))
 			})
 			.catch((err) => {
 				console.log('err: ', err);
@@ -46,64 +81,31 @@ const YourProfit = (pros) => {
 			loadBenefits();
 		},
 		//eslint-disable-next-line react-hooks/exhaustive-deps
-		[ symbol, qtyForm, sideForm ]
+		[symbol, qtyForm, sideForm]
 	);
+
+	const myArrow = ({ type, onClick }) => {
+		const pointer = type === consts.PREV ? <i className="fas fa-chevron-left"></i> : <i className="fas fa-chevron-right"></i>
+		return <button className='profits-arrows' onClick={onClick}>{pointer}</button>
+	}
+
+	if (profits === '') return null
 
 	return (
 		<section className="your-profit">
 			<div>
-				<h2>Your Profit</h2>
+				<h2>Your Profits</h2>
 			</div>
+
 			<div className="your-profit-data">
-				<div className="exchange">
-					<div className="name">
-						<span
-							style={{
-								color: 'rgb(240, 185, 11)',
-								fontWeight: 'bold'
-							}}
-						>
-							BINANCE
-						</span>
-					</div>
-					<div className="numbers">
-						<span>+ {binance.pct} % </span>
-						<span>+ {binance.btc} BTC</span>
-					</div>
-				</div>
-				<div className="exchange">
-					<div className="name center">
-						<span
-							style={{
-								color: '#0a6970',
-								fontWeight: 'bold'
-							}}
-						>
-							POLONIEX
-						</span>
-					</div>
-					<div className="numbers center">
-						<span>+ {poloniex.pct} % </span>
-						<span>+ {poloniex.btc} BTC</span>
-					</div>
-				</div>
-				<div className="exchange">
-					<div className="name">
-						<span
-							style={{
-								color: '#0084d4',
-								fontWeight: 'bold'
-							}}
-						>
-							BITTREX
-						</span>
-					</div>
-					<div className="numbers">
-						<span>+ {bittrex.pct} % </span>
-						<span>+ {bittrex.btc} BTC</span>
-					</div>
-				</div>
-			</div>
+			<Carousel renderArrow={myArrow} itemsToShow={1} renderPagination={() => {return <div></div>}
+			}>
+				{profits}
+
+			</Carousel>
+
+				
+			</div> 
 		</section>
 	);
 };
