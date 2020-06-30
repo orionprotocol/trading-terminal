27a981a9ce9a10565e8a28b3c0f4fc8f7fca8527
Simon Kruse <kruse.simon@gmx.net>
2020-06-30 14:19:19 +0200

    Improve performance

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Fragment } from 'react';
-import Router from './Router';
-import Services from './services';
+import React, { lazy, Fragment, Suspense } from "react";
+import Router from "./Router";
+import "./css/common.scss";
 
-import './css/common.scss';
+const Services = lazy(() =>
+  import(/* webpackChunkName: 'Services' */ "./services")
+);
 
 function App() {
-    return (
-        <Fragment>
-            <Services/>
-            <Router />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Suspense fallback="">
+        <Services />
+      </Suspense>
+      <Router />
+    </Fragment>
+  );
 }
 
 export default App;
